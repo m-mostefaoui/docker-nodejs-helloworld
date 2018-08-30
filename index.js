2cfe73cec3b8777f89b10599d27245be62cd496a
Miloud Mostefaoui <m.mostefaoui@hotmail.fr>
2018-08-30 16:04:28 +0100

    Change message (#3)
    * New release
    
    * New Release
    
    * Message
    
    * Automation

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var PORT = process.env.PORT || DEFAULT_PORT;
 // App
 var app = express();
 app.get('/', function (req, res) {
-  res.send('Hello World Release\n');
+  res.send('Hello World Release 2\n');
 });
 
 app.listen(PORT)
